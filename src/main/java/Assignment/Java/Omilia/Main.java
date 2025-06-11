e412efd6eaaefcc711b76936d045833f7ca10b00
TimTim <tima.sniezhkov1@gmail.com>
2025-06-11 21:14:47 +0200

    refactor: cosmetic changes and refactor validation in single method

--- src/main/java/Assignment/Java/Omilia/Main.java
+++ src/main/java/Assignment/Java/Omilia/Main.java
@@ -8,32 +8,47 @@ public class Main {
         static final String PHONE_NUMBER_VALID = "[phone number: VALID]";
         static final String PHONE_NUMBER_INVALID = "[phone number: INVALID]";
         static final String TEN_SYMBOLS_PATTERN = "(^2\\d{9}$)" + "|" + "(^69\\d{8}$)";
-        static final String FOURTEEN_SYMBOLS_PATTER = "(^00302\\d{9}$)" + "|" + "(^003069\\d{8}$)";
+        static final String FOURTEEN_SYMBOLS_PATTERN = "(^00302\\d{9}$)" + "|" + "(^003069\\d{8}$)";
     }
     public static void main(String[] args) {
-        int enteredStringLength;
-        String enteredStringNoSpace;
+        String input, inputNumber;
+        StringBuilder numberBuilder = new StringBuilder();
 
         System.out.println(ConstValues.WELCOME_STRING);
         Scanner scanner = new Scanner(System.in);
 
-        enteredStringNoSpace = scanner.nextLine().replaceAll(" ","");
-        enteredStringLength = enteredStringNoSpace.length();
+        input = scanner.nextLine().trim();
+        for (String part : input.split("\\s+"))
+            if (part.matches("\\d{1,3}")) {
+                numberBuilder.append(part);
+            } else {
+                System.err.println("Invalid part of number: " + part);
+                return;
+            }
 
-        System.out.print(enteredStringNoSpace + "\t");
+        inputNumber = numberBuilder.toString();
+        System.out.print(inputNumber + "\t");
 
-        switch (enteredStringLength){
+        if (isValidGreekNumber(inputNumber))
+            System.out.println(ConstValues.PHONE_NUMBER_VALID);
+        else
+            System.out.println(ConstValues.PHONE_NUMBER_INVALID);
+    }
+
+    public static boolean isValidGreekNumber(String number){
+
+        int inputStringLength = number.length();
+
+        switch (inputStringLength){
             case 10 -> {
-                if (enteredStringNoSpace.matches(ConstValues.TEN_SYMBOLS_PATTERN))
-                    System.out.println(ConstValues.PHONE_NUMBER_VALID);
-                else System.out.println(ConstValues.PHONE_NUMBER_INVALID);
+                return number.matches(ConstValues.TEN_SYMBOLS_PATTERN);
             }
             case 14 -> {
-                if (enteredStringNoSpace.matches(ConstValues.FOURTEEN_SYMBOLS_PATTER))
-                    System.out.println(ConstValues.PHONE_NUMBER_VALID);
-                else System.out.println(ConstValues.PHONE_NUMBER_INVALID);
+                return number.matches(ConstValues.FOURTEEN_SYMBOLS_PATTERN);
             }
-            default -> System.out.println(ConstValues.PHONE_NUMBER_INVALID);
+            default -> {
+                return false;
+            }
         }
     }
 }
