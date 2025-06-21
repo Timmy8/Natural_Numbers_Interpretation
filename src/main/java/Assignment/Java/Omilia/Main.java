34f5974c70354168b550cf6760648a9687409e56
TimTim <tima.sniezhkov1@gmail.com>
2025-06-21 15:58:28 +0200

    creation: first code part of advanced task

--- src/main/java/Assignment/Java/Omilia/Main.java
+++ src/main/java/Assignment/Java/Omilia/Main.java
@@ -1,5 +1,8 @@
 package Assignment.Java.Omilia;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -13,29 +16,22 @@ public class Main {
         private ConstValues(){}
     }
 
+
     public static void main(String[] args) {
-        String input, inputNumber;
-
+        String input, inputAsNumber;
         System.out.println(ConstValues.WELCOME_STRING);
-        Scanner scanner = new Scanner(System.in);
 
-        input = scanner.nextLine().trim();
-
-        try {
-            inputNumber = createNumberFromInput(input);
+        try (Scanner scanner = new Scanner(System.in);){
+            input = scanner.nextLine();
+            inputAsNumber = createNumberFromInput(input);
         } catch (RuntimeException ex){
             System.err.println(ex.getMessage());
             return;
         }
 
-        System.out.print(inputNumber + "\t");
+        System.out.println(inputAsNumber + "\t" + (isValidGreekNumber(inputAsNumber)? ConstValues.PHONE_NUMBER_VALID : ConstValues.PHONE_NUMBER_INVALID));
 
-        if (isValidGreekNumber(inputNumber))
-            System.out.println(ConstValues.PHONE_NUMBER_VALID);
-        else
-            System.out.println(ConstValues.PHONE_NUMBER_INVALID);
-
-        scanner.close();
+        getAllPossibleNumbers(input);
     }
 
     public static boolean isValidGreekNumber(String number){
@@ -58,7 +54,7 @@ public class Main {
     public static String createNumberFromInput(String input){
         StringBuilder numberBuilder = new StringBuilder();
 
-        for (String part : input.split("\\s+"))
+        for (String part : input.trim().split("\\s+"))
             if (part.matches("\\d{1,3}")) {
                 numberBuilder.append(part);
             } else {
@@ -66,5 +62,50 @@ public class Main {
             }
 
         return numberBuilder.toString();
+    }
+
+    public static List<String> getAllPossibleNumbers(String input){
+        List<List<String>> options = new ArrayList<>();
+        String[] tokens = input.split("\\s");
+        List<Integer> parsedParts = Arrays.stream(tokens).mapToInt(Integer::parseInt).boxed().toList();
+
+        for (int i = 0; i < parsedParts.size(); i++){
+            List<String> variants = new ArrayList<>();
+            int currentPart = parsedParts.get(i);
+            int nextPart = (i + 1 < parsedParts.size())?parsedParts.get(i + 1) : Integer.MAX_VALUE;
+
+            variants.add(String.valueOf(currentPart));
+
+            if ((currentPart % 100 == 0 && nextPart < 100) || (currentPart % 10 == 0 && nextPart < 10)){
+                variants.add(String.valueOf(currentPart + nextPart));
+
+                System.out.println("i: " + variants);
+            }
+
+            if (currentPart > 20 && currentPart < 100  && currentPart % 10 != 0){
+                String firstVariant = String.valueOf((currentPart / 10) * 10);
+                String secondVariant = String.valueOf(currentPart % 10);
+                variants.add(firstVariant + " " + secondVariant);
+
+                System.out.println("i: " + variants);
+            } else if (currentPart > 100 && currentPart < 1000  && currentPart % 100 != 0){
+                String firstVariant = String.valueOf((currentPart / 100) * 100);
+                String secondVariant = String.valueOf(currentPart % 100);
+                variants.add(firstVariant + " " + secondVariant);
+
+                System.out.println("i: " + variants);
+            }
+
+            options.add(variants);
+//            if (parsedParts[i] > 10 && parsedParts[i] < 100 && parsedParts[i] % 10 != 0){
+//                String possiblePart = ((parsedParts[i] / 10) * 10) + " " + (parsedParts[i] % 10);
+//                System.out.println("pp3: " + parsedParts[i] + " : " + possiblePart);
+//            } else if (parsedParts[i] > 100 && parsedParts[i] < 1000 && parsedParts[i] % 100 != 0){
+//                String possiblePart = ((parsedParts[i] / 100) * 100) + " " + (parsedParts[i] % 100);
+//                System.out.println("pp4: " + parsedParts[i] + " : " + possiblePart);
+//            }
+        }
+
+        return possibleNumbers;
     }
 }
